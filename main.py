2e0baf7d6149c4db59a00f09204a3ca68e8fb743
Gerard <dothanhvinh1@gmail.com>
2023-05-11 12:11:49 +0700

    Add url endpoint and all data

--- main.py
+++ main.py
@@ -18,6 +18,20 @@ def about(station, date):
     tempearture = df.loc[df["    DATE"] == date][ '   TG'].squeeze() / 10
     return {"station": station, "temperature": tempearture, "date": date}
 
+@app.route("/api/v1/<station>/")
+def all_stations(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df.loc[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
